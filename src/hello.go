074288bdc103e983474abce3f5429b8fe197f2a1
David Stinnette <dastinnette@gmail.com>
2017-12-08 09:49:35 -0700

    Variadic function

--- src/hello.go
+++ src/hello.go
@@ -8,12 +8,19 @@ type Salutation struct {
 }
 
 func Greet(salutation Salutation) {
-	fmt.Println(salutation.name)
-	fmt.Println(salutation.greeting)
+	message, alternate := CreateMessage(salutation.name, salutation.greeting, "yo")
+
+	fmt.Println(message)
+	fmt.Println(alternate)
+}
+
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+	message = greeting[1] + " " + name
+	alternate = "HEY!" + name
+	return
 }
 
 func main() {
 	var s = Salutation{"Bob", "Hello"}
 	Greet(s)
-
 }
